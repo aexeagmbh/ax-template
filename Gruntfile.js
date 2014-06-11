d77d64a25b9544d6bb3a2c8b69044f8065cbe3d9
Tillmann Karras <tilkax@gmail.com>
2014-06-11 17:21:24 +0200

    Switch from compass to libsass/grunt
    This doesn't require ruby and grunt let's us do more stuff during the
    build. All required JavaScript files (and uncompressed ones for easy
    debugging) are now correctly copied from bower_components/.

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'bower_components/foundation/js/',    src: '**',                   dest: 'js/'},
+          {expand: true, cwd: 'bower_components/foundation/css/',   src: 'normalize.css',        dest: 'css/'},
+          {expand: true, cwd: 'bower_components/foundation-icons/', src: 'foundation-icons.css', dest: 'css/'},
+        ],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('build', ['sass', 'copy']);
+  grunt.registerTask('default', ['build', 'watch']);
+}
