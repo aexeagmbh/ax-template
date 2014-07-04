70105912a4cdd4e306f076df2f0afcb272bd8e7b
Benjamin Bretz <Benjamin.Bretz@aexea.de>
2014-07-04 09:44:36 +0200

    last commit lacked some files. Grunt: add possibility to run jshint; concat and compress js to ax-template.js and ax-template.min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,75 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: [
-          'bower_components/foundation/scss',
-          'bower_components/foundation-icon-fonts'
-        ]
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+        sass: {
+            options: {
+                includePaths: [
+                    'bower_components/foundation/scss',
+                    'bower_components/foundation-icon-fonts'
+                ]
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
         },
-        files: {
-          'css/app.css': 'scss/app.scss'
+
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
+
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass']
+            }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'bower_components/foundation/js/',    src: '**',                   dest: 'js/'},
+                    {expand: true, cwd: 'bower_components/foundation-icons/', src: 'foundation-icons.css', dest: 'css/'}
+                ]
+            }
+        },
+
+        jshint: {
+            all: ['Gruntfile.js', 'js/**/*.js'],
+            jshintrc: true
+        },
+
+        concat: {
+            "ax-template": {
+                dest: 'js/ax-template.js',
+                src: [
+                    'bower_components/foundation/js/vendor/jquery.js',
+                    'bower_components/foundation/js/vendor/modernizr.js',
+                    'bower_components/foundation/js/foundation.js'
+                ]
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/ax-template.js',
+                dest: 'js/ax-template.min.js'
+            }
         }
-      }
-    },
+    });
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    },
 
-    copy: {
-      main: {
-        files: [
-          {expand: true, cwd: 'bower_components/foundation/js/',    src: '**',                   dest: 'js/'},
-          {expand: true, cwd: 'bower_components/foundation-icons/', src: 'foundation-icons.css', dest: 'css/'},
-        ],
-      },
-    },
-  });
-
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  grunt.registerTask('build', ['sass', 'copy']);
-  grunt.registerTask('default', ['build', 'watch']);
+    grunt.registerTask('buildJs', ['concat', 'uglify']);
+    grunt.registerTask('build', ['sass', 'buildJs', 'copy']);
+    grunt.registerTask('default', ['build', 'watch']);
 }
